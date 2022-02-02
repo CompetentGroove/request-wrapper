26b8c1d40786220b471d18b2d1dfb4eb751bb88a
Nitin Rajput <Nitin Rajput@GROOVE.COM>
2022-02-02 17:22:15 +0530

    first-commit

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ const axios=require('axios');
 async function get(url,callback) {
     try {
       const response = await axios.get(url);
-      callback(response);
-      console.log(response.data);
+      callback(response.status,response.data); 
     } catch (error) {
       console.error(error);
     }
   }
-
-
-  
+ 
 
  async function post(url,callback) {
     try {
-    let response = await axios.post(url);
-    callback(response);
-     
+    let response = await axios.post(url)
+    callback(response.status,response.data); 
     } catch (error) {
         console.log(error)
     }
@@ -28,8 +24,7 @@ async function get(url,callback) {
 async function Delete(url) {
     try{
     let res = await axios.delete(url);
-    
-    console.log( res.data);
+    callback(response.status,response.data);
         
     } catch (error) {
         console.log(error)
@@ -37,35 +32,26 @@ async function Delete(url) {
 }
 
 
-
-
-
 async function put(url,callback){
     try {
          const response= await axios.put(url);
-         callback(response);
+         callback(response.status,response.data);
+
     } catch (error) {
         console.log(error);  
     }
 }
 
 
-
-
-
 async function patch(url,callback){
   try {
   const response= await axios.patch(url);
-   callback(response)
+  callback(response.status,response.data);
+
   } catch (error) {
     console.log(error)
   }
 }
-
-
-
-
-
 
 module.exports={get,post,put,Delete,patch};
 
