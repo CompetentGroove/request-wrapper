e926dbf739cb6054c138661cc967aa7611f83201
nitinrajput129 <niti@competentgroove.com>
2022-02-08 10:01:57 +0530

    some-change

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ async function get(url, callback) {
   }
 }
 
+async function request(options, callback) {
+  let res, err, status;
+  try {
+    const response = await axios(options);
+    res = response.data;
+    status = response.status;
+  } catch (error) {
+    err = error;
+    res = err.response.data;
+    status = err.status;
+  } finally {
+    callback(err, status, res);
+  }
+}
+
+
+
+
+
 async function post(url, data, callback) {
   let res, err, status;
   try {
@@ -28,7 +47,6 @@ async function post(url, data, callback) {
     callback(err, status, res);
   }
 }
-
 
 
 async function put(url, data,callback) {
